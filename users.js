49da970f84fe093e8976427d621bbd7e03f22cf2
motaz khogali <108723690+motazKhogali@users.noreply.github.com>
2023-08-24 14:55:31 +0200

    add user db

---
+++ users.js
@@ -0,0 +1,25 @@
+const {con,closeCon} = require('./db');
+
+async function createUser(userName,password){
+    const client = await con();
+    const len = await client.collection('users').countDocuments();
+    await client.collection('users').insertOne({
+        name:userName,password:password,user_id:len
+    });
+    await closeCon();
+}
+
+async function findUser(userName){
+    const client = await con();
+    const user = await client.collection('users').findOne({
+        name:userName,
+    });
+    await closeCon();
+    console.log(user)
+    return user;
+}
+
+
+module.exports = {
+    createUser,findUser
+}
